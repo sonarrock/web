6a48a23b2e7f499dcc5a3124becc48a4624dcd57
sonarrock <cmrm1982@gmail.com>
2025-08-19 02:26:17 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,13 @@
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open("sonarrock-cache").then(cache => {
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("sonarrock-v1").then((cache) => {
       return cache.addAll([
         "/",
         "/index.html",
-        "/styles.css",
-        "/icons/icon-192.png",
-        "/icons/icon-512.png"
+        "/manifest.json",
+        "/icons/android-chrome-192x192.png",
+        "/icons/android-chrome-512x512.png"
       ]);
-    })
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
     })
   );
 });

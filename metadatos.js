20b61ef8d9e8325ddb2bcd1401e2417dbff19c3d
sonarrock <cmrm1982@gmail.com>
2025-09-06 01:08:10 -0600

    Delete metadatos.js

--- metadatos.js
+++
@@ -1,7 +0,0 @@
-// No se muestran metadatos ni mensajes porque Zeno.fm no los expone.
-async function loadITunesMetadata() {
-  const nowPlaying = document.getElementById('now-playing');
-  nowPlaying.textContent = ""; // Área completamente limpia
-}
-
-document.addEventListener('DOMContentLoaded', loadITunesMetadata);

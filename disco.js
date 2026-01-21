1ea316d3bb608a80c20f5ae1c64a84b472c8ffb3
sonarrock <cmrm1982@gmail.com>
2026-01-21 02:07:39 -0600

    Update disco.js

--- disco.js
+++ disco.js
@@ -1,7 +1,7 @@
 // ----------------------------
 // DISCO DE LA SEMANA
 // ----------------------------
-const fileName = "Fleetwood Mac - Gold Dust Woman.mp3"; // cambiar cada semana
+const fileName = "Fleetwood Mac - Romours.mp3"; // cambiar cada semana
 const discoAudio = document.getElementById("disco-audio");
 const discoProgress = document.getElementById("disco-progress");
 const discoProgressContainer = document.getElementById("disco-progress-container");

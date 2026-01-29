dd0da026b21391baa1c8f5b6df572a5ea31ae372
sonarrock <cmrm1982@gmail.com>
2026-01-28 19:55:26 -0600

    Update player.js

--- player.js
+++ player.js
@@ -3,14 +3,35 @@ document.addEventListener("DOMContentLoaded", () => {
   const audio = document.getElementById("radio-audio");
   const playBtn = document.getElementById("playPauseBtn");
 
-  console.log("JS CARGADO", audio, playBtn);
+  const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
 
-  const STREAM_URL = "https://stream.zeno.fm/ezq3fcuf5ehvv";
+  const STREAM_URL = isIOS
+    ? "https://node-01.zeno.fm/ezq3fcuf5ehvv"
+    : "https://stream.zeno.fm/ezq3fcuf5ehvv";
+
+  let isPlaying = false;
 
   playBtn.addEventListener("click", () => {
-    console.log("CLICK PLAY");
-    audio.src = STREAM_URL;
-    audio.play().catch(err => console.error(err));
+
+    if (!isPlaying) {
+      audio.src = STREAM_URL;
+      audio.load();
+
+      setTimeout(() => {
+        audio.play().then(() => {
+          isPlaying = true;
+          playBtn.innerHTML = '<i class="fas fa-pause"></i>';
+        }).catch(err => {
+          console.error("iOS bloqueó el audio:", err);
+        });
+      }, 300);
+
+    } else {
+      audio.pause();
+      isPlaying = false;
+      playBtn.innerHTML = '<i class="fas fa-play"></i>';
+    }
+
   });
 
 });

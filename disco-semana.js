535490cd0dae22a07727d9e08455558c456e9d56
sonarrock <cmrm1982@gmail.com>
2026-01-28 21:14:07 -0600

    Update disco-semana.js

--- disco-semana.js
+++ disco-semana.js
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-
   const fileName = "Aretha Franklin - Lady Soul.mp3";
-
   const audio = document.getElementById("disco-audio");
   const cover = document.getElementById("cover");
   const title = document.getElementById("track-title");
@@ -9,11 +7,6 @@ document.addEventListener("DOMContentLoaded", () => {
   if (!audio || !cover || !title) return;
 
   title.textContent = fileName.replace(".mp3", "");
-
-  audio.src =
-    "https://raw.githubusercontent.com/sonarrock/web/main/El%20Disco%20De%20La%20Semana/" +
-    encodeURIComponent(fileName);
-
-  cover.src =
-    "https://raw.githubusercontent.com/sonarrock/web/main/El%20Disco%20De%20La%20Semana/portada.jpg";
+  audio.src = "https://raw.githubusercontent.com/sonarrock/web/main/El%20Disco%20De%20La%20Semana/" + encodeURIComponent(fileName);
+  cover.src = "https://raw.githubusercontent.com/sonarrock/web/main/El%20Disco%20De%20La%20Semana/portada.jpg?v=" + Date.now();
 });

f05e34f8c915bd9fdadf4b975743ddc3e7e7d2af
sonarrock <cmrm1982@gmail.com>
2026-01-28 21:00:26 -0600

    Update radio-player.js

--- radio-player.js
+++ radio-player.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-
   const audio = document.getElementById("radio-audio");
   const playBtn = document.getElementById("playPauseBtn");
   const stopBtn = document.getElementById("stop-btn");
   const muteBtn = document.getElementById("mute-btn");
   const volume = document.getElementById("volume");
 
+  // URL del streaming
   audio.src = "https://stream.zeno.fm/ezq3fcuf5ehvv";
   audio.volume = 1;
 
   playBtn.onclick = () => {
-    audio.play();
+    audio.play().catch(e => console.log("Play bloqueado por navegador:", e));
     playBtn.innerHTML = '<i class="fas fa-pause"></i>';
   };
 
